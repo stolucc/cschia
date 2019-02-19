bde07041fa350c120505b29b77b3d552b786466b
izabella197 <juliagrabos@gmail.com>
2019-02-19 11:27:08 +0000

    Changing date foramts for presentation

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, \
-TextAreaField, IntegerField, SelectField
-from wtforms.fields.html5 import DateField
+TextAreaField, IntegerField, SelectField, DateField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length, \
 Optional
 from app.models import User
